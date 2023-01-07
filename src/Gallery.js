811be83ccc3585c86bf72199fed706af9b3a6842
Ricrado Gonçalves <ricar.goncalves@gmail.com>
2023-01-07 16:26:40 +0000

    Swap to react-photo-album and yet-another-react-lightbox

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,82 +1,82 @@
-import React, { Component, Fragment } from 'react';
-import { useParams } from 'react-router-dom';
-import ReactPhotoGallery from 'react-photo-gallery';
-// Custom components
-import Carousel from './Carousel';
-import Thumbnail from './Thumbnail';
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import PropTypes from 'prop-types';
 
-function withParams(Component) {
-    return props => <Component {...props} params={useParams()} />;
-}
+import PhotoAlbum from "react-photo-album";
 
-class Gallery extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            photos: [],
-            selectedImage: 0,
-            viewerIsOpen: false
-        };
-        
-        this.currentAlbum = null;
-    }
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
 
-    fetchPhotos(collection, album) {
+// import optional lightbox plugins
+import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
+import Slideshow from "yet-another-react-lightbox/plugins/slideshow";
+import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+import Video from "yet-another-react-lightbox/plugins/video";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
+
+function Gallery({zoom}) {
+    const {collection, album} = useParams();
+    const [photos, setPhotos] = useState([]);
+    const [index, setIndex] = useState(-1);
+
+    useEffect(() => {
         // Clear gallery when a new album is selected
-        this.setState({ photos: [] });
+        setPhotos([]);
+
         fetch(`/collection/${collection}/album/${album}`)
             .then((response) => response.json())
             .then(album => {
-                this.setState({ photos: album.photos });
-            }); 
-    }
+                setPhotos(album.photos);
+            });
+    }, [collection, album]);
 
-    componentDidMount() {
-        let { collection, album } = this.props.params;
-        this.fetchPhotos(collection, album);
-    }
-
-    shouldComponentUpdate(nextProps) {
-        if(nextProps.params.album !== this.currentAlbum) {
-            this.currentAlbum = nextProps.params.album;
-            this.fetchPhotos(nextProps.params.collection, nextProps.params.album);
-            return false;
-        }
-        return true;
-    }
-    
-    openLightbox = (event, { photo, index }) => {
-        this.setState({
-            selectedImage: index,
-            viewerIsOpen: true
-        });
-    };
-    
-    closeLightbox = () => {
-        this.setState({
-            viewerIsOpen: false
-        });
-    };
-    
-    render() {
-        return (
-            <Fragment>
-                <ReactPhotoGallery photos={this.state.photos} onClick={this.openLightbox} targetRowHeight={120} margin={1} renderImage={Thumbnail} />
-                <Carousel
-                    selected={this.state.selectedImage}
-                    open={this.state.viewerIsOpen}
-                    onClose={this.closeLightbox}
-                    items={this.state.photos
-                    //     .map(photo => ({
-                    //     originalTitle: photo.title,
-                    //     description  : photo.title,
-                    //     original: photo.files[0].url,
-                    //     thumbnail: photo.src,
-                    // }))
-                    } />
-            </Fragment>
-        );
-    }
+    return (
+        <>
+            <PhotoAlbum
+                photos={photos}
+                layout="rows"
+                targetRowHeight={zoom}
+                spacing={1}
+                onClick={({ index }) => setIndex(index)} />
+            <Lightbox
+                slides={photos.map(({ src, width, height }) => ({
+                    src,
+                    width: 2000,
+                    height: 2000,
+                    srcSet: [{
+                        src: src,
+                        width: 200,
+                        height: 200,
+                    }],
+                }))}
+                open={index >= 0}
+                index={index}
+                animation={{ swipe: 150 }}
+                close={() => setIndex(-1)}
+                // enable optional lightbox plugins
+                plugins={[Fullscreen, Slideshow, Thumbnails, Video, Zoom]}
+                carousel={{
+                    finite: true,
+                    preload: 3,
+                    padding: 0,
+                    spacing: 0,
+                    imageFit: "contain"
+                }}
+                thumbnails={{
+                    position: "bottom",
+                    width: 80,
+                    height: 80,
+                    borderRadius: 0,
+                    padding: 0,
+                    gap: 2
+                  }} />
+        </>
+    );
 }
 
-export default withParams(Gallery);
+Gallery.propTypes = {
+    zoom: PropTypes.number,
+}
+
+export default Gallery;
