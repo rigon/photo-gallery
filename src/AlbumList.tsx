72e6724455d427c4a166433d4e5406634a4875be
Ricardo Gonçalves <ricar.goncalves@gmail.com>
2023-01-25 01:21:36 +0000

    Loading indicator for albums

--- src/AlbumList.tsx
+++ src/AlbumList.tsx
@@ -1,8 +1,10 @@
 import { FC, useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
+import Box from '@mui/material/Box';
 import List from '@mui/material/List';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
+import LinearProgress from '@mui/material/LinearProgress';
 import Typography from '@mui/material/Typography';
 
 import { AlbumType } from "./types";
@@ -14,20 +16,23 @@ interface AlbumListProps {
 
 const AlbumList: FC<AlbumListProps> = ({onClick}) => {
     const { collection, album } = useParams();
+    const [isLoading, setLoading] = useState<boolean>(true);
     const [albums, setAlbums] = useState<AlbumType[]>([]);
 
     useEffect(() => {
+        setLoading(true);
         // Check if collection is valid
         if(collection !== undefined && collection.length > 0) {
             fetch(`/collection/${collection}/albums`)
                 .then((response) => response.json())
                 .then(albums => {
                     setAlbums(albums);
+                    setLoading(false);
                 });
         }
     }, [collection]);
-
-    return (
+    
+    const list = (
         <List onClick={onClick}>
         { albums.map((a, index) => (
             <ListItemButton key={a.name} component={Link} to={`/${collection}/${a.name}`} selected={a.name === album}>
@@ -36,8 +41,14 @@ const AlbumList: FC<AlbumListProps> = ({onClick}) => {
                 </ListItemText>
             </ListItemButton>
         ))}
-        </List>
-    );
+        </List>);
+    
+    const loading = (
+        <Box sx={{ width: '100%' }}>
+            <LinearProgress />
+        </Box>);
+
+    return (isLoading ? loading : list);
 }
 
 export default AlbumList;
