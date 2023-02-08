e063df41ed4765cafe5a0348d8f91d46eebe7d4d
Ricardo Gonçalves <ricar.goncalves@gmail.com>
2023-02-08 04:43:40 +0000

    outDir vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
+import { build, defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import eslint from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "build",
+  },
   plugins: [react(), eslint()],
   server: {
     port: 3000,
