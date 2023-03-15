6e4345ddea7b4dc79398c13bdd631d6173f7b8df
Ricardo Gonçalves <ricar.goncalves@gmail.com>
2023-03-15 00:34:24 +0000

    New sections in gallery

--- src/AlbumList.tsx
+++ src/AlbumList.tsx
@@ -3,16 +3,21 @@ import { useParams, Link } from 'react-router-dom';
 import Box from '@mui/material/Box';
 import ClearIcon from '@mui/icons-material/Clear';
 import IconButton from '@mui/material/IconButton';
+import Divider from '@mui/material/Divider';
 import InputAdornment from '@mui/material/InputAdornment';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import LinearProgress from '@mui/material/LinearProgress';
+import PeopleIcon from '@mui/icons-material/PeopleAlt';
+import PinDropIcon from '@mui/icons-material/PinDrop';
 import SearchIcon from '@mui/icons-material/Search';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 
+import TimelineIcon from './icons/TimelineIcon';
 import { useGetAlbumsQuery } from "./services/api";
 
 interface AlbumListProps {
@@ -37,25 +42,54 @@ const AlbumList: FC<AlbumListProps> = ({onClick}) => {
     };
     
     const list = (<>
-        <TextField
-            label="Search albums"
-            value={searchTerm}
-            onChange={onSearch}
-            fullWidth
-            variant="filled"
-            size="small"
-            InputProps={{
-                startAdornment: (
-                    <InputAdornment position="start">
-                        <SearchIcon />
-                    </InputAdornment>),
-                endAdornment: (
-                    <InputAdornment position="end">
-                        <IconButton edge="end" onClick={clearSearch}>
-                            <ClearIcon />
-                        </IconButton>
-                    </InputAdornment>),
-                }} />
+        <List>
+            <ListItem disablePadding>
+                <ListItemButton>
+                    <ListItemIcon>
+                        <TimelineIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Timeline" />
+                </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+                <ListItemButton>
+                    <ListItemIcon>
+                        <PeopleIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="People" />
+                </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+                <ListItemButton>
+                    <ListItemIcon>
+                        <PinDropIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Places" />
+                </ListItemButton>
+            </ListItem>
+            <Divider />
+            <ListItem disablePadding>
+                <TextField
+                    label="Search albums"
+                    value={searchTerm}
+                    onChange={onSearch}
+                    fullWidth
+                    variant="filled"
+                    size="small"
+                    InputProps={{
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <SearchIcon />
+                            </InputAdornment>),
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <IconButton edge="end" onClick={clearSearch}>
+                                    <ClearIcon />
+                                </IconButton>
+                            </InputAdornment>),
+                        }} />
+            </ListItem>
+        </List>
         <List onClick={onClick}>
             { albums.map((a) => (
                 <ListItem key={a.name} disablePadding>
